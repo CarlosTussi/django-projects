4e5f1e9fc304c1d5489948ff34efc041b3220010
Carlos Eduardo Tussi <cetleite@gmail.com>
2023-01-20 18:12:59 +0100

    [ADD] Example with Models

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    ISBN = models.IntegerField(primary_key= True)
+    name = models.CharField(max_length=30)
+    author = models.CharField(max_length=30)
+    year = models.IntegerField()
+
+
+    def __str__(self):
+        return "\nISBN: {}\nName: {}\nAuthor: {}\nYear: {}\n".format(self.ISBN, self.name, self.author, self.year)
